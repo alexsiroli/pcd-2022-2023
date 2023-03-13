422fa5dac97ff4aa48912dce0f0e7409a83aa4e0
Alex Siroli <alex.siroli@studio.unibo.it>
2023-03-13 13:49:08 +0100

    es #1 done

--- app/src/main/java/pcd/lab02/lost_updates/Worker.java
+++ app/src/main/java/pcd/lab02/lost_updates/Worker.java
@@ -3,17 +3,21 @@ package pcd.lab02.lost_updates;
 
 public class Worker extends Thread {
 	
-	private UnsafeCounter counter;
-	private int ntimes;
+	private final UnsafeCounter counter;
+	private final int ntimes;
+	private final Object lock;
 	
-	public Worker(UnsafeCounter c, int ntimes){
-		counter = c;
+	public Worker(UnsafeCounter c, int ntimes, Object lock){
+		this.counter = c;
 		this.ntimes = ntimes;
+		this.lock = lock;
 	}
 	
 	public void run(){
-		for (int i = 0; i < ntimes; i++){
-			counter.inc();
+		for (int i = 0; i < this.ntimes; i++){
+			synchronized (this.lock) {
+				this.counter.inc();
+			}
 		}
 	}
 }
